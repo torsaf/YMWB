863d7c77615863cfd2d3d3daa052c3504d0054c2
torsaf <4405544@gmail.com>
2025-11-13 01:31:30 +0300

    убрал дубли из новых товаров

--- unlisted.py
+++ unlisted.py
@@ -10,27 +10,35 @@ def generate_unlisted():
         with sqlite3.connect('System/marketplace_base.db') as mp_conn:
             mp_df = pd.read_sql_query("SELECT Sklad, Invask, Okno, United FROM marketplace", mp_conn)
 
-        # Собираем все артикулы в один список
+        # Объединяем и нормализуем артикулы
         listed_articles = pd.concat([
             mp_df['Sklad'],
             mp_df['Invask'],
             mp_df['Okno'],
             mp_df['United']
-        ], ignore_index=True).dropna().unique()
+        ], ignore_index=True).fillna('').astype(str)
+
+        # Очистка от пробелов, табов и ведущих нулей
+        listed_articles = listed_articles.str.replace(r'\s+', '', regex=True).str.lstrip('0')
+        listed_articles = listed_articles[listed_articles != ''].unique()
 
         # Подключение к базе всех товаров
         with sqlite3.connect('System/!YMWB.db') as all_conn:
             all_df = pd.read_sql_query("SELECT * FROM prices", all_conn)
 
-        # Отбор: не выложенные и не от 'Sklad'
+        # Нормализуем артикулы в базе всех товаров
+        all_df['Артикул'] = all_df['Артикул'].astype(str).fillna('').str.replace(r'\s+', '', regex=True).str.lstrip('0')
+
+        # Фильтрация: исключаем уже имеющиеся и склад
         not_listed_df = all_df[
             (~all_df['Артикул'].isin(listed_articles)) &
-            (all_df['Поставщик'] != 'Sklad')
+            (all_df['Поставщик'].str.lower() != 'sklad')
         ]
 
-        return not_listed_df
+        return not_listed_df.reset_index(drop=True)
 
     except Exception as e:
         logger.error(f"Ошибка в generate_unlisted(): {e}", exc_info=True)
         return pd.DataFrame()
+
 
